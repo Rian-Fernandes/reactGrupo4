d09ea550efed2e68db1e109cb86970cc52ed7b05
Rian-Fernandes <rian10fernandes@hotmail.com>
2023-11-04 18:35:03 -0300

    First commit - Commit com erros - Refazer na madruga

--- projetoGrupo4/src/App.jsx
+++ projetoGrupo4/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home/";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company/";
+import NewProject from "./components/pages/NewProject/";
+
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
