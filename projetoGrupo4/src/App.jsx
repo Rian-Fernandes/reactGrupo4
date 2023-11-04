d65cf55bf09441a526f49f9025d555c9d07eb934
leignel <cristian.leignel@gmail.com>
2023-11-04 14:47:50 -0300

    components footer

--- projetoGrupo4/src/App.jsx
+++ projetoGrupo4/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
