96b9211cef78796e48945aaaa3cfadf067253d06
PalomaPts <141692596+PalomaPts@users.noreply.github.com>
2023-11-06 01:39:10 -0300

    Botão de saída

--- projetoGrupo4/src/components/Pages/Home.jsx
+++ projetoGrupo4/src/components/Pages/Home.jsx
@@ -1,28 +1,30 @@
-import styles from './Home.module.css'
-import { useEffect } from 'react';
-import woman from '../../Images/woman.png'
-import LinkButton from '../layout/LinkButton';
+import styles from "./Home.module.css";
+import { useEffect } from "react";
+import woman from "../../Images/woman.png";
+import LinkButton from "../layout/LinkButton";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Button from "../Button";
 
 function Home() {
-    useEffect ( () =>{
-        document.body.style = 'background-image: linear-gradient(to right, #576851 0%, #576851 50%, #A58567 50%, #A58567 100%);';
-    },[])
+  useEffect(() => {
+    document.body.style =
+      "background-image: linear-gradient(to right, #576851 0%, #576851 50%, #A58567 50%, #A58567 100%);";
+  }, []);
   return (
     <>
-    <section className={styles.home_container}>
+      <section className={styles.home_container}>
         <h1>
-            Bem vindo ao <span>Flor & Folha</span>
+          Bem vindo ao <span>Flor & Folha</span>
         </h1>
         <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-        <LinkButton to="/newproject" text="Cadastrar"/>
-        <img src={woman}/>
-    </section>
+        <LinkButton to="/newproject" text="Cadastrar" />
+        <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+          Sair
+        </Button>
+      </section>
     </>
-  )
-     
+  );
 }
 
 export default Home;
-
-
-
