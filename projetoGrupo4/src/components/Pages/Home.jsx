f2d1ae8d3f466f1d0029f090b0ed7dc54589058c
leignel <cristian.leignel@gmail.com>
2023-11-06 18:05:25 -0300

    Imagem background home

--- projetoGrupo4/src/components/Pages/Home.jsx
+++ projetoGrupo4/src/components/Pages/Home.jsx
@@ -4,14 +4,17 @@ import woman from "../../Images/woman.png";
 import LinkButton from "../layout/LinkButton";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import Navbar from "../layout/Navbar";
+import Footer from "../layout/Footer";
 
 function Home() {
   useEffect(() => {
     document.body.style =
-      "background-image: linear-gradient(to right, #576851 0%, #576851 50%, #A58567 50%, #A58567 100%);";
+      "background-image: linear-gradient(to right, #576851 0%, #576851 50%, #A58567 50%, #A58567 100%),";
   }, []);
   return (
     <>
+      <Navbar />
       <section className={styles.home_container}>
         <h1>
           Bem vindo ao <span>Flor & Folha</span>
@@ -20,6 +23,7 @@ function Home() {
         <LinkButton to="/newproject" text="Cadastrar" />
         <img src={woman} />
       </section>
+      <Footer />
     </>
   );
 }
