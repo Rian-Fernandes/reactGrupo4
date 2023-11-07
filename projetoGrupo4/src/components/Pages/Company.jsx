513a13a1c3e735b8d325d42307b59411ef623a67
leignel <cristian.leignel@gmail.com>
2023-11-07 14:53:46 -0300

    Página da empresa

--- projetoGrupo4/src/components/Pages/Company.jsx
+++ projetoGrupo4/src/components/Pages/Company.jsx
@@ -1,15 +1,81 @@
-import { useEffect } from "react";
+import styles from "./Company.module.css";
 import Navbar from "../layout/Navbar";
 import Footer from "../layout/Footer";
+
 function Company() {
-  useEffect(() => {
-    document.body.style = "background: #576851";
-  }, []);
   return (
     <>
       <Navbar />
-      <section>
-        <h1>Company</h1>
+      <section id={styles.categorias}>
+        <span className={styles.categoriaTitle}>Sobre a Flor & Folha</span>
+        <span className={styles.categoriaDesc}>
+          Somos à Flor & Folha, onde a beleza da natureza encontra a praticidade
+          da gestão de produtos. Somos uma empresa dedicada a tornar a
+          experiência de gerenciar plantas e produtos relacionados tão simples e
+          eficiente quanto possível. Com uma paixão pela diversidade botânica,
+          oferecemos uma ampla variedade de produtos que podem transformar seu
+          espaço em um oásis verde.
+        </span>
+        <br />
+        <span className={styles.categoriaDesc}>
+          Na Flor & Folha, acreditamos que a natureza é uma fonte infinita de
+          inspiração. Estamos aqui para ajudá-lo a elevar a gestão de seus
+          produtos, tornando a conexão com a natureza mais acessível e
+          encantadora do que nunca. Comece agora a explorar nossas categorias de
+          produtos e descubra o que a Flor & Folha tem a oferecer. Seja
+          bem-vindo ao nosso mundo de beleza botânica e praticidade.
+        </span>
+        <br />
+        <span className={styles.categoriaTitle}>
+          Nossas Categorias de Produtos:
+        </span>
+        <div className={styles.categoriaBars}>
+          <div className={styles.categoriaBar}>
+            <div className={styles.categoriaBarText}>
+              <h3>Cactos</h3>
+              <p>
+                Descubra a beleza única dos cactos, com suas formas intrigantes
+                e resistência surpreendente. Nossa seleção de cactos é perfeita
+                para quem procura plantas de baixa manutenção com um toque de
+                exotismo.
+              </p>
+            </div>
+          </div>
+          <div className={styles.categoriaBar}>
+            <div className={styles.categoriaBarText}>
+              <h3>Suculentas</h3>
+              <p>
+                As suculentas são conhecidas por sua capacidade de armazenar
+                água e vêm em uma variedade de formas e cores. Elas são ideais
+                para adicionar um toque de verde em espaços pequenos ou para
+                quem gosta de plantas de fácil cuidado.
+              </p>
+            </div>
+          </div>
+          <div className={styles.categoriaBar}>
+            <div className={styles.categoriaBarText}>
+              <h3>Bonsai</h3>
+              <p>
+                Encante-se com a arte dos bonsais, árvores em miniatura que
+                representam harmonia e equilíbrio. Nossos bonsais são
+                cuidadosamente cultivados para trazer a elegância desta tradição
+                japonesa ao seu lar ou jardim.
+              </p>
+            </div>
+          </div>
+          <div className={styles.categoriaBar}>
+            <div className={styles.categoriaBarText}>
+              <h3>Plantas de Jardim</h3>
+              <p>
+                Se você está buscando plantas para transformar seu jardim em um
+                paraíso natural, explore nossa seleção de plantas de jardim.
+                Desde flores deslumbrantes até plantas perenes resistentes,
+                temos tudo o que você precisa para criar um espaço ao ar livre
+                incrível.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </>
--- projetoGrupo4/src/components/layout/Navbar.jsx
+++ projetoGrupo4/src/components/layout/Navbar.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import Container from "./Container";
 import styles from "./Navbar.module.css";
-import logoflora from '../../../public/logoflora.png'
+import logoflora from "../../../public/logoflora.png";
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Container>
         <Link to="/home">
-          <img src={logoflora} style={{width: '50px', height: 'auto'}}/>
+          <img src={logoflora} style={{ width: "50px", height: "auto" }} />
         </Link>
         <ul className={styles.list}>
           <li className={styles.item}>
@@ -17,14 +17,9 @@ function Navbar() {
           <li className={styles.item}>
             <Link to="/projects">Produtos</Link>
           </li>
-          <li className={styles.item}>
-            <Link to="/contact">Contato</Link>
-          </li>
+
           <li className={styles.item}>
             <Link to="/company">Empresa</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to="/newproject">Novo produto</Link>
           </li>
         </ul>
         <hr className={styles.line} />
